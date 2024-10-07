419f283840775b6ccfcc6e51cc76ac168cfa97b4
Sarvesh1593 <sarvesh.in.2003@gmail.com>
2024-10-07 07:25:32 +0530

    Pushing the project

---
+++ routes/postRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { createPost, deletePost, getPost, likeUnlikePost, replyToPost ,getFeedPost, getUserPosts} from '../controllers/postController.js'
+import protectRoute from '../middlewares/protectRoutes.js'
+
+const router= express.Router()
+
+router.get("/feed",protectRoute,getFeedPost)
+router.get("/:id",getPost)
+router.get("/user/:username",getUserPosts)
+router.post("/create", protectRoute,createPost)
+router.delete("/:id",protectRoute,deletePost)
+router.put("/like/:id",protectRoute,likeUnlikePost)
+router.put("/reply/:id",protectRoute,replyToPost)
+
+export default router
+
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import connectDB from './db/connect_DB.js';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
+import postRoutes from './routes/postRoutes.js'
+import {v2 as cloudinary} from "cloudinary"
+import cors from 'cors'
+
+dotenv.config()
+const PORT = process.env.PORT || 3001
+
+const app = express()
+connectDB()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+app.use(cors({
+  origin: "https://user-connect.vercel.app",  // Allow requests from this origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
+}));
+
+app.use(express.json({limit :'50mb'})); // To parse Json data in req.body
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
+app.listen(PORT, ()=> console.log(`Server is started at https://localhost:${PORT}`))
